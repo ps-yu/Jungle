85883b9b7f6f7237b66d29da5aca8b5e9e0b2439
ps-yu <pankajsharma12540@gmail.com>
2023-05-01 14:12:37 -0400

    Implement user authentication

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,33 +1,16 @@
-class Admin::ProductsController < ApplicationController
-  http_basic_authenticate_with :name => ENV["USERNAME"], :password => ENV["PASSWORD"]
+class Admin::DashboardController < ApplicationController
+  # http_basic_authenticate_with :name => ENV["USERNAME"], :password => ENV["PASSWORD"]
 
-  def index
-    @products = Product.order(id: :desc).all
-@@ -12,7 +13,7 @@ def create
-    @product = Product.new(product_params)
+  before_filter :authorize
 
-    if @product.save
-      redirect_to [:admin, :products], notice: "Product created!"
-    else
-      render :new
-    end
-@@ -21,7 +22,7 @@ def create
-  def destroy
-    @product = Product.find params[:id]
-    @product.destroy
-    redirect_to [:admin, :products], notice: "Product deleted!"
+  def cool
   end
 
-  private
-  def product_params
-    params.require(:product).permit(
-      :name,
-      :description,
-      :category_id,
-      :quantity,
-      :image,
-      :price
-    )
+  def free
   end
 
+  def show
+    @products = Product.all
+    @categories = Category.all
+  end
 end
