c70633f5e222c5fe82649561764edab09c64f094
ps-yu <pankajsharma12540@gmail.com>
2023-05-01 14:15:05 -0400

    app/controllers/categories_controller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,43 @@
-class CategoriesController < ApplicationController
+class Admin::CategoriesController < ApplicationController
+  # http_basic_authenticate_with :name => ENV["USERNAME"], :password => ENV["PASSWORD"]
 
-  def show
-    @category = Category.find(params[:id])
-    @products = @category.products.order(created_at: :desc)
+  before_filter :authorize
+
+  def cool
   end
 
+  def free
+  end
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: "Category created!"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @category = Category.find params[:id]
+    @category.destroy
+    redirect_to [:admin, :categories], notice: "Category deleted!"
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+    )
+  end
 end
